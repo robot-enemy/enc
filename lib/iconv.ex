6f895697f6de4919a16173cdc3a8da58f0defd8e
Matthew Slater <delameko@delameko-stone.com>
2021-11-20 14:14:49 +0000

    Initial commit

---
+++ lib/iconv.ex
@@ -0,0 +1,18 @@
+defmodule Iconv do
+  @moduledoc """
+  Documentation for `Iconv`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Iconv.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/iconv_test.exs
@@ -0,0 +1,8 @@
+defmodule IconvTest do
+  use ExUnit.Case
+  doctest Iconv
+
+  test "greets the world" do
+    assert Iconv.hello() == :world
+  end
+end
