c50f8cea3880276dd467c4bfb0a50267c5d9a427
Matthew Slater <delameko@delameko-stone.com>
2021-11-22 11:06:06 +0000

    Add convert_string_encoding/3 and convert_file_encoding/3

--- lib/iconv.ex
+++ lib/iconv.ex
@@ -2,14 +2,21 @@ defmodule Iconv do
   @moduledoc """
   A set of function wrappers around the `iconv` command line utility.
   """
+  @convert_script_path Path.join(__DIR__, "../scripts/convert-string")
   @detect_script_path  Path.join(__DIR__, "../scripts/detect-string")
 
+  def convert_file_encoding(file_path, from_enc, to_enc) do
+    System.cmd("iconv", ["-f #{from_enc}", "-t #{to_enc}", file_path])
+  end
 
+  def convert_string_encoding(str, from_enc, to_enc) do
+    System.cmd(@convert_script_path, [str, from_enc, to_enc])
+  end
 
   def detect_file_encoding(file_path) do
     case System.cmd("uchardet", [file_path]) do
       {encoding, 0} -> {:ok, String.trim(encoding)}
-      {_, 1} -> {:error, :detection_failed}
+      {_, 1}        -> {:error, :detection_failed}
     end
   end
 
